4f2f5c591569eee6baf78ae7eea8bbeef25cca5e
Benjamin Negrevergne <Benjamin.Negrevergne@imag.fr>
2010-09-09 13:59:21 -0400

    Added forgotten clogen.hpp file

---
+++ clogen.hpp
@@ -0,0 +1,17 @@
+//
+// clogen.hpp
+// 
+// Made by Benjamin Negrevergne
+// Email   <@[firstname].[name]@@@imag.fr@>
+// 
+// Started on  Fri Sep  3 14:44:21 2010 Benjamin Negrevergne
+//
+
+#ifndef   	_CLOGEN_HPP_
+#define   	_CLOGEN_HPP_
+
+#include "pattern.hpp"
+
+void expand(set_t c); 
+
+#endif	    /* _CLOGEN_HPP_ */
