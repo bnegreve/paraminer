7edb344a74d580c40448029f1a25bfc8ca241328
Benjamin Negrevergne <Benjamin.Negrevergne@imag.fr>
2011-01-12 15:52:23 +0100

    BUG : graduals, forgot to change + to - in database reading

--- clogen_local_graduals.cpp
+++ clogen_local_graduals.cpp
@@ -112,15 +112,11 @@ int tt_to_grad_items(TransactionTable *output, const TransactionTable &input){
 	t.original_tid=nb_trans++; 
 	t.reserve(nb_attributes); 
 	for(int k = 0; k < input[i].size(); k++){
-	  if(input[i][k] < input[j][k]){
-	    t.push_back(2*k+1); 
-	  }
-	  else if(input[i][k] > input[j][k]){
+	  if(input[i][k] <= input[j][k]){
 	    t.push_back(2*k); 
 	  }
-	  else{
-	    	    t.push_back(2*k);
-		    t.push_back(2*k+1); 
+	  else if(input[i][k] >= input[j][k]){
+	    t.push_back(2*k+1); 
 	  }
 	}
 	output->push_back(t); 
