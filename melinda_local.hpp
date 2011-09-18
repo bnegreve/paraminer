b5e1596a00378313e5924c9463c4f5db8ff85c7d
Benjamin.Negrevergne@imag.fr <bengreve@idrouille.imag.fr>
2011-09-18 20:11:29 +0200

    added few other strategies, same strat is always default

--- melinda_local.hpp
+++ melinda_local.hpp
@@ -26,13 +26,51 @@ typedef struct{
   int u_data; 
 }tuple_t; 
 
+#define STRAT_STANDARD
+
+#ifdef STRAT_USE_LESS_MEM
 int m_distribute(opaque_tuple_t *tuple){
-  return m_thread_id(); 
+  tuple_t *t = (tuple_t*)tuple; 
+  return  ((long int)t->tt)%64; 
+  //  return 0; 
+}
+
+
+int m_retrieve(){
+  return 1;
+}
+#endif
+
+
+#ifdef STRAT_BIG_FIRST
+int m_distribute(opaque_tuple_t *tuple){
+  tuple_t *t = (tuple_t*)tuple;
+  
+  //  printf("DBSIZE %d\n", ((long int)t->tt->size())); 
+  if(((long int)t->tt->size()) > 500){    
+    return 0; 
+  } 
+  if(((long int)t->tt->size()) > 100){    
+    return 1; 
+  } 
+  return 2; 
+}
+
+
+int m_retrieve(){
+  return 0;
+}
+#endif
+
+
+#ifdef STRAT_STANDARD
+int m_distribute(opaque_tuple_t *tuple){
+    return m_thread_id(); 
 }
 
 int m_retrieve(){
-  return m_thread_id(); 
+    return m_thread_id(); 
 }
-
+#endif
 
 #endif	    /* _MELINDA_LOCAL_HPP_ */
