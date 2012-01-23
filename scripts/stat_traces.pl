7a2170a768ad75c10ac5f5933a57bc6af9a36d0a
Benjamin Negrevergne <Benjamin.Negrevergne@imag.fr>
2012-01-23 19:01:31 +0100

    Added script files to compute statistics from trace file data

---
+++ scripts/stat_traces.pl
@@ -0,0 +1,87 @@
+#!/usr/bin/perl
+
+# computes event delays in paraminer trace files (diff between START
+# EVENT XXX end END EVENT XXX). Outputs are column formated and can be
+# directly fed into stat.pl.
+
+# USAGE: for an input file input.trace crated with the following command
+# $> cat > input.trace
+# 0, 0, START global
+# 0, 0, START chicken
+# 12, 0, END chicken
+# 0, 0, START gluc
+# 13, 0, END gluc
+# 14, 0, START gluc
+# 18, 0, END gluc
+# 25, 0, END global
+# 40, 0, START chicken
+# 45, 0, END chicken
+
+# The following command 
+# $> ./stat_traces.pl < input.traces | ./stats.pl
+# will output 
+#
+# gluc global chicken 
+# COLUMN 0 : SUM = 17, SIZ = 2, AVG = 8.5, STD = 4.5
+# COLUMN 1 : SUM = 25, SIZ = 1, AVG = 25, STD = 0
+# COLUMN 2 : SUM = 17, SIZ = 2, AVG = 8.5, STD = 3.5
+#
+#
+# see stats.pl for more details about it. 
+
+
+
+%output; 
+while($line = <STDIN>){
+    if($line =~ /^\s*#/){next;}
+    chomp $line; 
+
+    @input = split(/[\t ]*,[\t ]*/,  $line);
+
+    if($#input != 2){
+	die "input format is <timestamp in s>, <thread id>, <event name>\n"; 
+    }
+    
+    $time = $input[0]; 
+    $thread_id = $input[1]; 
+    $event_name = $input[2]; 
+
+    if($event_name =~ /START[ \t]*(.*)/){
+	if(defined $events{$1} and $events{$1} != -1){
+	    print STDERR "Warning START END sequence not respected\n"; 
+	}
+	$events{$1} = $time; 
+    }
+    elsif($event_name =~ /END[ \t]*(.*)/){	
+	$delay = $time - $events{$1}; 
+	$events{$1} = -1; 
+	push @{$output{$1}}, $delay; 
+    }
+}
+
+$max_event = 0; 
+
+#print field names
+print "#";
+foreach $k (keys %output){
+    print "$k "; 
+    if( $#{$output{$k}}+1 > $max_event){
+	$max_event = $#{$output{$k}} + 1; 
+    }
+}
+print "\n";
+
+#print field values
+for($i = 0; $i < $max_event; $i++){
+    foreach $k (keys %output){
+	if(defined  $output{$k}[$i]){
+	    print $output{$k}[$i]." "; 
+	}
+	else {
+	    print "ukn ";
+	}
+	    
+    }
+    print "\n"
+}
+
---
+++ scripts/stats.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+# reads column formated numeric data on stdin, and prints some simple statistics
+# eg. 
+# ./stats.pl < 
+# 10 2 100
+# 15 3 100
+
+# outputs 
+# COLUMN 0 : SUM = 25, SIZ = 2, AVG = 12.5, STD = 2.5
+# COLUMN 1 : SUM = 5, SIZ = 2, AVG = 2.5, STD = 0.5
+# COLUMN 2 : SUM = 200, SIZ = 2, AVG = 100, STD = 0
+# where SUM is the sum of all elements in the column 
+# SIZ is the # on elements in one columns 
+# AVG is the average 
+# STD is the standard deviation 
+
+#comments in input file are transmited to error output
+@transposed = (); 
+
+# $line = <STDIN> or die $!; 
+# @input = split(/ /, $line);
+
+while($line = <STDIN>){
+    if($line =~ /^\s*#/){print STDERR $line; next;}
+    chomp $line; 
+    @input = split(/ +/,  $line);
+    
+
+    for($cur = 0; $cur <= $#input; $cur++){
+	if($input[$cur] =~ /[-+]?([0-9]*\.)?[0-9]+/){
+	    push @{$transposed[$cur]}, $input[$cur]; 
+	}
+    }
+}
+
+$n = 0; 
+foreach $i (@transposed){
+    $sum = 0; 
+    foreach $j(@{$i}){
+	$sum+= $j; 
+    }
+    print "COLUMN $n : "; 
+    print "SUM = $sum, "; 
+    $size  = ($#{$i}+1); 
+    print "SIZ = $size, ";
+    $avg = $sum / $size; 
+    print "AVG = $avg, ";
+    
+    $var = 0; 
+    foreach $j(@{$i}){
+	$var += ($avg - $j) * ($avg - $j); 
+    }
+    $var = $var / $size; 
+    $std = sqrt($var); 
+    
+    print "STD = $std" ; 
+    print "\n"; 
+$n++; 
+}
+
+
